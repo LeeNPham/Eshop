50c8d01fac64e48401d9fdcfd250a2d5508ba697
Lee Pham <leenpham@gmail.com>
2022-08-11 12:41:14 -0700

    Initial Commit

--- store/models/product.py
+++ store/models/product.py
@@ -2,10 +2,11 @@ from django.db import models
 from .category import Category
 class Products(models.Model):
     name = models.CharField(max_length=60)
-    price= models.IntegerField(default=0)
+    price= models.DecimalField(max_digits=10, decimal_places=2, default=0)
     category= models.ForeignKey(Category,on_delete=models.CASCADE,default=1 )
     description= models.CharField(max_length=250, default='', blank=True, null= True)
     image= models.ImageField(upload_to='uploads/products/')
+
 
     @staticmethod
     def get_products_by_id(ids):
@@ -19,4 +20,6 @@ class Products(models.Model):
         if category_id:
             return Products.objects.filter (category=category_id)
         else:
-            return Products.get_all_products();
+            return Products.get_all_products()
+
+    
